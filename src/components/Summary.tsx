b32fa652552282fcc4b0d483235da4055e546396
Dmitriy Gaev <dgaev@calltouch.net>
2026-08-08 11:47:44 +0500

    fix text

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -29,11 +29,11 @@ export function Summary({ totals, targets, daysInMonth, onOpenTargets, onOpenExp
           {/*<div className="summary__period">Расчёт на {daysInMonth} дней</div>*/}
           <div className="summary__buttons">
             <button className="button button--ghost summary__small-button" type="button" onClick={onOpenExport}>
-              Экспорт отчета в md
+              Отчет в md
             </button>
             <button className="button button--ghost summary__small-button" type="button" onClick={onOpenTargets}>
               <Icon name="edit" size={15} />
-              Настроить цели нутриентов
+              Цели нутриентов
             </button>
           </div>
         </div>
